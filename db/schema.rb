cf27162015f54ec809c348d24a3cfc7fe0fbb6bf
naufal.pratama1 <naufal.pratama@munalively.com>
2022-06-21 06:13:01 +0700

    waste model & migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_221658) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_20_225921) do
   create_table "users", force: :cascade do |t|
     t.string "nama"
     t.string "no_hp"
@@ -22,4 +22,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_221658) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "wastes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "jenis_sampah"
+    t.float "berat"
+    t.text "gambar"
+    t.text "alamat_penjemputan"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wastes_on_user_id"
+  end
+
+  add_foreign_key "wastes", "users"
 end
