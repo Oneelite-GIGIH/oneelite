3f27c4f68ece352bbfd58d076a236d9a71821247
mhnaufal <aal.pirates16@gmail.com>
2022-06-22 04:40:53 +0700

    update transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,4 +4,16 @@ class Transaction < ApplicationRecord
   belongs_to :waste
 
   belongs_to :delivery_courier
+
+  enum status: { NEW: 0, DELIVERED: 1, PAID: 2, CANCELLED: 3 }
+
+  validates :user, presence: true
+
+  validates :waste, presence: true
+
+  validates :delivery_courier, presence: true
+
+  validates :nominal_dibayar, presence: true, numericality: { greater_than: 0 }
+
+  validates :status, presence: true, inclusion: { in: statuses }
 end
