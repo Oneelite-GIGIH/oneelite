420290fd7644f415b24c8ab8b25204537b75be22
naufal.pratama1 <naufal.pratama@munalively.com>
2022-06-21 07:27:22 +0700

    user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
     enum role: { PENGGUNA: 0, KURIR: 1 }
+
+    validates :nama, presence: true
+
+    validates :no_hp, presence: true
+
+    validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Format Email Salah" }
+
+    validates :role, presence: true, inclusion: { in: roles }
+
+    validates :saldo, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+    validates :alamat, presence: true
 end
