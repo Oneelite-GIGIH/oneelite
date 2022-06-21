ea5de5513d3b09f4d44fc81ba8c8a5b5bfd01560
naufal.pratama1 <naufal.pratama@munalively.com>
2022-06-22 04:13:07 +0700

    update transaction model unit test

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe Transaction, type: :model do
     it '[model.transaction.2] is valid with valid transaction data' do
       user = User.new(nama: "Nama Test 2", no_hp: "+62 881234567", email: "test2@gmail.com", role: 0, saldo: 99.999, alamat: "Test Jalan 2")
       waste = Waste.new(user: user, jenis_sampah: "Makanan Sisa", berat: 3, gambar: "/transaction/gambar/sampah_1.png", alamat_penjemputan: "Jl. Test 2")
-      transaction = Transaction.new(user: user, waste: waste, nominal_dibayar: 10000, status: 0)
+      delivery_courier = DeliveryCourier.new(user: user)
+      transaction = Transaction.new(user: user, waste: waste, delivery_courier: delivery_courier, nominal_dibayar: 10000, status: 0)
       transaction.valid?
       expect(transaction).to be_valid
     end
