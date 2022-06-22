23f6ca6c40e1b9cdabea73d8c94cfbf2b4d132d4
suryaharahap <suryaharahap18@gmail.com>
2022-06-22 13:35:00 +0700

    Create layout homepages and setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root "home_pages#index"
+
+  get "home", to: "home_pages#index"
+  # get 'home_pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
