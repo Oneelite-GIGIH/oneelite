1917981925c77918fde84489e81933f247eb98dd
suryaharahap <suryaharahap18@gmail.com>
2022-06-22 16:06:52 +0700

    Create Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "home_pages#index"
-
+  
   get "home", to: "home_pages#index"
+  get  "/signup",  to: "users#new"
+
   # get 'home_pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
