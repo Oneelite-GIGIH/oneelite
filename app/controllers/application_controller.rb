8d34ace8d335f39f52541492edde46a80a76432f
mhnaufal <aal.pirates16@gmail.com>
2022-06-26 20:14:28 +0700

    helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :user_is_logged_in?
+
+  def user_is_logged_in?
+    !!session[:user_id]
+  end
 end
