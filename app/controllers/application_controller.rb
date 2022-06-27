2fcee86bf8dec05f346c5860bbfe0afedd75684c
mhnaufal <aal.pirates16@gmail.com>
2022-06-27 22:01:17 +0700

    create helper for json response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :user_is_logged_in?
+  helper_method :user_is_logged_in? 
+  helper_method :send_success
+  helper_method :send_failed
 
   def user_is_logged_in?
     !!session[:user_id]
+  end
+
+  def send_success(message, payload)
+    {
+      status: "SUCCESS",
+      url: request.host,
+      message: message,
+      payload: payload
+    }
+  end
+
+  def send_failed(message, payload = nil)
+    {
+      status: "FAILED",
+      url: request.host,
+      message: message,
+      payload: payload
+    }
   end
 end
