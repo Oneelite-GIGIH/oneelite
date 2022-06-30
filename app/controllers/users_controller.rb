82201ea33728abdcae1feb349ded30ac2353af52
mhnaufal <aal.pirates16@gmail.com>
2022-06-30 17:29:00 +0700

    fix signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @user = User.find_by(id: current_user)
     render :json => {:message => @user}
@@ -6,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    render :json => {:message => @user}
   end
 
   def new
@@ -13,8 +16,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    # TODO: why always getting role as PENGGUNA?
     @user = User.new(user_params)
     if @user.save
+      if user_params[:role] == 1 || user_params[:role] == "1" || @user.role == 1 || @user.role == "KURIR"
+        @delivery_courier = DeliveryCourier.new(user: @user)
+        @delivery_courier.save
+      end
       flash[:success] = "Welcome to the Oneelite!"
       redirect_to @user
     else
@@ -25,7 +33,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:nama, :no_hp, :email, :alamat, :password, :password_confirmation)
+      params.require(:user).permit(:nama, :no_hp, :email, :alamat, :role.to_s.to_i, :password, :password_confirmation)
     end
 
 end
